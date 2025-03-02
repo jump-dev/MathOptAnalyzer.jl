84f20bc3ba9425b3d8176f168468027b86f0ac14
Joaquim <joaquimdgarcia@gmail.com>
2025-03-02 19:53:12 -0300

    Update ModelAnalyzer.jl

--- src/ModelAnalyzer.jl
+++ src/ModelAnalyzer.jl
@@ -1,2 +1,7 @@
+# Copyright (c) 2025: Joaquim Garcia, Oscar Dowson and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 module ModelAnalyzer
 end
