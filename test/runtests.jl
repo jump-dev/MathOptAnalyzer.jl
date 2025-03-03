2b868d683ad6a559fe963df252ef456cbcaabd93
joaquimg <joaquimdgarcia@gmail.com>
2025-03-02 23:43:33 -0300

    deps and test basics

---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+# Copyright (c) 2025: Joaquim Garcia, Oscar Dowson and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
+import ModelAnalyzer
+import MathOptInterface as MOI
+using Test
+using JuMP
+
